66227b76470044c5561095ad8e63a04ca2e58fea
abdulrehman104 <130191723+abdulrehman104@users.noreply.github.com>
2024-04-30 12:02:58 +0500

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,178 @@
-import Image from "next/image";
 
-export default function Home() {
+// Get request to our API
+async function Home() {
+  const bookData = await fetch("https://simple-books-api.glitch.me/");
+  const res = await bookData.json();
+  console.log(res);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <h1>APIS</h1>
+    </>
+  )
+}
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+export default Home;
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+// Get data of our books
+async function Home() {
+  const bookData = await fetch("https://simple-books-api.glitch.me/books");
+  const res = await bookData.json();
+  console.log(res);
+  return (
+    <>
+      <h1>APIS</h1>
+    </>
+  )
+}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+export default Home;
+
+
+// Get data of sinlge book
+async function Home() {
+  const bookData = await fetch("https://simple-books-api.glitch.me/books/1");
+  const res = await bookData.json();
+  console.log(res);
+  return (
+    <>
+      <h1>APIS</h1>
+    </>
+  )
+}
+
+export default Home;
+
+
+
+// Post Method 
+// Post Method for Authentication:
+async function Home() {
+  const ordersData = await fetch('https://simple-books-api.glitch.me/api-clients/', {
+    method: 'POST',
+    headers: {
+      'content-Type' : 'application/json'
+    },
+    body: JSON.stringify(
+      {
+        clientName: "AbdulRehman",
+        clientEmail: "AbdulRehman@example.com"
+     }
+    ),
+  });
+  const res = await ordersData.json();
+  console.log(res);
+
+  // Output 
+  // {
+  //   accessToken: '880b47ec192535d3904b30dd1ec95171efbe014718566801530498ff6350d3b2'
+  // }
+
+  return (
+    <>
+      <h1>APIS</h1>
+    </>
   );
 }
+
+export default Home;
+
+
+
+
+// first we can chect our orders with the help of Get Request 
+async function Home() {
+  const ordersData = await fetch('https://simple-books-api.glitch.me/orders', {
+    headers: {
+      Authorization : 'Bearer 880b47ec192535d3904b30dd1ec95171efbe014718566801530498ff6350d3b2'
+    }
+  })
+  const res = await ordersData.json();
+  console.log(res);
+  return (
+    <>
+      <h1>APIS</h1>
+    </>
+  );
+}
+
+export default Home;
+
+
+
+// Then order a book with the help of POST method: 
+
+async function Home() {
+  const ordersData = await fetch('https://simple-books-api.glitch.me/orders', {
+    method:'POST',
+    headers: {
+      Authorization: `Bearer 880b47ec192535d3904b30dd1ec95171efbe014718566801530498ff6350d3b2`,
+    },
+    body: JSON.stringify(
+      {
+        "bookId": 1,
+        "customerName": "AbdulRehman"
+      }
+    )
+  })
+  const res = await ordersData.json();
+  console.log(res);
+  return (
+    <>
+      <h1>APIS</h1>
+    </>
+  );
+}
+
+export default Home;
+
+
+
+// Get data of our Orders 
+async function Home() {
+  const ordersData = await fetch('https://simple-books-api.glitch.me/orders', {
+    headers: {
+      Authorization: `Bearer 65e17e4e6c0ad72ce8eece6ec8aefb66ab1c98650f1f0c4d98866d07848c0fd9`,
+    },
+  });
+  const res = await ordersData.json();
+  console.log(res);
+  return (
+    <>
+      <h1>APIS</h1>
+    </>
+  );
+}
+
+export default Home;
+
+
+
+// Update our order with the help of Patch 
+async function Home() {
+  const ordersData = await fetch('https://simple-books-api.glitch.me/orders/2h3iwlz3YhEYQbRLOhRS6', {
+    method: 'Patch',
+    headers: {
+      Authorization: `Bearer 65e17e4e6c0ad72ce8eece6ec8aefb66ab1c98650f1f0c4d98866d07848c0fd9`,
+    },
+    body: JSON.stringify(
+      {
+        customerName: "Rehman"
+      }
+    )
+  });
+  const res = await ordersData.json();
+  console.log(res);
+  return (
+    <>
+      <h1>APIS</h1>
+    </>
+  );
+}
+
+export default Home;
+
+
+
+
